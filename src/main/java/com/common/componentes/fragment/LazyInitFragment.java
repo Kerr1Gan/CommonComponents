ab6906fe09251577ed5f97c18d712ec00aaccbf7
Kerr1Gan <674712964@qq.com>
2019-03-23 16:13:03 +0800

    add LazyInitFragment support java 8

---
+++ src/main/java/com/common/componentes/fragment/LazyInitFragment.java
@@ -0,0 +1,52 @@
+package com.common.componentes.fragment;
+
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class LazyInitFragment extends Fragment {
+
+    private static final String TAG = "LazyInitFragment";
+
+    private Handler mHandler;
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreate");
+        super.onCreate(savedInstanceState);
+        if (mHandler == null) {
+            mHandler = new Handler();
+        }
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onCreateView");
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        Log.i(TAG, "onViewCreated");
+        super.onViewCreated(view, savedInstanceState);
+    }
+
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        Log.i(TAG, "setUserVisibleHint " + isVisibleToUser);
+        super.setUserVisibleHint(isVisibleToUser);
+        mHandler.post(() -> {
+            onUserVisibleHintChanged(getUserVisibleHint());
+        });
+    }
+
+    public void onUserVisibleHintChanged(boolean isVisibleToUser) {
+        Log.i(TAG, "onUserVisibleHintChanged " + isVisibleToUser);
+    }
+}
